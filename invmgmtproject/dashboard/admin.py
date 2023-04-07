0da02a2ad24a089c266234f62e3fecea16ae77c8
Luis Marquez <lmarquez1@ncstudents.niagaracollege.ca>
2023-04-07 15:18:47 -0400

    user App and registration page

--- invmgmtproject/dashboard/admin.py
+++ invmgmtproject/dashboard/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Order
+from django.contrib.auth.models import Group
+
+admin.site.site_header = "MRQZ Company"
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'quantity')
+    list_filter = ['category']
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('product', 'order_qty', 'staff')
+    list_filter = ['staff']
+
 
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
+# admin.site.unregister(Group)
--- invmgmtproject/dashboard/models.py
+++ invmgmtproject/dashboard/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 CATEGORY = (
@@ -15,3 +16,19 @@ class Product(models.Model):
 
     def __str__(self):
         return f'{self.name} - {self.quantity}'
+
+    class Meta:
+        verbose_name_plural = 'Product'
+
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    staff = models.ForeignKey(User, models.CASCADE, null=True)
+    order_qty = models.PositiveIntegerField(null=True)
+    order_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.product} ordered by {self.staff.username} '
+
+    class Meta:
+        verbose_name_plural = 'Order'
