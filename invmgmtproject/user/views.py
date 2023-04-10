5616f484ddc797a82a794c6aa9fb49db9193257a
Luis Marquez <lmarquez1@ncstudents.niagaracollege.ca>
2023-04-10 13:11:08 -0400

    Added image and profile

--- invmgmtproject/user/views.py
+++ invmgmtproject/user/views.py
@@ -18,3 +18,7 @@ def register(request):
         'form': form,
     }
     return render(request, 'user/register.html', context)
+
+
+def profile(request):
+    return render(request, 'user/profile.html')
