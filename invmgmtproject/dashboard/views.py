7342b947e494e00faba39225a4ce5a4adc9dafb5
Luis Marquez <lmarquez1@ncstudents.niagaracollege.ca>
2023-04-12 16:30:57 -0400

    Order View added

--- invmgmtproject/dashboard/views.py
+++ invmgmtproject/dashboard/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import Product
+from .models import Product, Order
 from .forms import ProductForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -14,7 +15,20 @@ def index(request):
 
 @login_required
 def staff(request):
-    return render(request, 'dashboard/staff.html')
+    workers = User.objects.all()
+    context = {
+        'workers': workers
+    }
+    return render(request, 'dashboard/staff.html', context)
+
+
+@login_required
+def staff_detail(request, pk):
+    workers = User.objects.get(id=pk)
+    context = {
+        'workers': workers
+    }
+    return render(request, 'dashboard/staff_detail.html', context)
 
 
 @login_required
@@ -35,6 +49,7 @@ def product(request):
     return render(request, 'dashboard/product.html', context)
 
 
+@login_required
 def product_delete(request, pk):
     item = Product.objects.get(id=pk)
     if request.method == 'POST':
@@ -43,6 +58,7 @@ def product_delete(request, pk):
     return render(request, 'dashboard/product_delete.html')
 
 
+@login_required
 def product_update(request, pk):
     item = Product.objects.get(id=pk)
     if request.method == 'POST':
@@ -61,4 +77,9 @@ def product_update(request, pk):
 
 @login_required
 def order(request):
-    return render(request, 'dashboard/order.html')
+    orders = Order.objects.all()
+
+    context = {
+        'orders': orders
+    }
+    return render(request, 'dashboard/order.html', context)
